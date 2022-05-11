10053eb0777eac1707d6339780b40269a23f6171
Matteo Cordioli <79856894+MatteoCordioli@users.noreply.github.com>
2022-05-11 16:00:39 +0200

    FooterComponent

---
+++ components/TheFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="link-primary text-center">
+    <a class="test">
+      <nuxt-link :to="pippo.path">
+        {{ pippo.name }}
+      </nuxt-link>
+    </a>
+    <p class="polimiText">Politecnico Di Milano <br>Hypermedia Application - 2022</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheFooterComponent',
+  data() {
+    return {
+      pippo: {
+        name: 'ContactUs',
+        path: '/about',
+      },
+    }
+  },
+}
+</script>
+<style scoped>
+.polimiText{
+  color:grey;
+  font-style: bold;
+}
+a {
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+</style>
