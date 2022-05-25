309fbbac2c19f6fbb2e2713a7dab64da37315c41
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2022-05-25 11:23:54 +0200

    Add default error page

---
+++ src/layouts/error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container position-absolute top-50 start-50 translate-middle">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <p>{{error.message}}</p>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ErrorPage',
+  layout: 'defaultLayout',
+  props: {
+      error: {
+          type: Object,
+          required: true,
+          }
+  },
+}
+</script>
+
+
+<style scoped>
+.container{
+    text-align: center;
+}
+</style>
