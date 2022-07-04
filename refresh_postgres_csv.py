4799f6d7ad3db9e16c33d314f00aa38f1527fd74
andreacerasani <andrea.cerasani@mail.polimi.it>
2022-07-04 16:17:15 +0200

    changed nuxt.config.js for new url and added redirect-ssl

--- refresh_postgres_csv.py
+++ refresh_postgres_csv.py
@@ -9,7 +9,10 @@ conn = psycopg2.connect("dbname=hyp user=postgres password=postgres")
 cur = conn.cursor()
 
 ############### PRODUCTION ################
+###### hype-project
 # conn = psycopg2.connect("postgres://nafvdfbljgnzzg:adaee8f8a86a01de1a92a3f727c323b03eef292b80624f5ddde41316bb730f3e@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/da71int9ee1nqm", sslmode='require')
+###### visitvenice
+# conn = psycopg2.connect("postgres://qhwwutikrbyqnd:a90f530c02ffec2dc949cea806c947c81a76fc557073d9f7ba84303e3296f965@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/d7th3tpiqogeai", sslmode='require')
 # cur = conn.cursor()
 # cur.execute("SET DateStyle='dmy'")
 
